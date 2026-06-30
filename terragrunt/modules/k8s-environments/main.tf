9925f1fb412f90b1679ea07aff98d515708c62b7
OleksiiTaran <alexey4ik007@gmail.com>
2026-06-30 17:30:48 +0200

    update main.tf

--- terragrunt/modules/k8s-environments/main.tf
+++ terragrunt/modules/k8s-environments/main.tf
@@ -36,7 +36,14 @@ provider "kubernetes" {
   cluster_ca_certificate = kind_cluster.cluster.cluster_ca_certificate
 }
 
-provider "helm" {}
+provider "helm" {
+  kubernetes {
+    host                   = kind_cluster.cluster.endpoint
+    client_certificate     = kind_cluster.cluster.client_certificate
+    client_key             = kind_cluster.cluster.client_key
+    cluster_ca_certificate = kind_cluster.cluster.cluster_ca_certificate
+  }
+}
 
 resource "kubernetes_namespace" "env" {
   metadata {
